f527de7e93b7fe7940ebfcc120ce8bbf57d209aa
Tatsuto Higa <n18021@std.it-colege.ac.jp>
2018-06-21 03:02:48 +0000

    6/20

---
+++ 0620/062001.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in 'seq 1 100'; do
+	  if [ $(($i % 3 )) == 0 -a $(($i % 5)) == 0 ]; then
+		  echo "FIZZ"	
+   	elif [ $(($i % 3)) == 0 ]; then
+	  	echo "FIZZ"
+	elif [ $(($i % 5)) == 0 ]; then
+		echo "BUZZ"
+ else
+	echo "$i"
+	fi
+done
